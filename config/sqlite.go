36071ac14d74d613434b03b17f66ef689650f097
Gian <50121981+zenndahl@users.noreply.github.com>
2024-01-29 15:30:19 -0300

    feat: config with logger and sqlite

---
+++ config/sqlite.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"gobooks/schemas"
+	"os"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+func InitiSQLite() (*gorm.DB, error) {
+	logger := GetLogger("sqlite")
+	dbPath := "./db/books.db"
+
+	_, err := os.Stat(dbPath)
+	if os.IsNotExist(err) {
+		logger.Info("database file not found, creating...")
+		err = os.MkdirAll("./db", os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		file, err := os.Create(dbPath)
+		if err != nil {
+			return nil, err
+		}
+		file.Close()
+	}
+
+	db, err := gorm.Open(sqlite.Open(dbPath), &gorm.Config{})
+	if err != nil {
+		logger.Errf("sqlite opening error: %v", err)
+		return nil, err
+	}
+	err = db.AutoMigrate(&schemas.Opening{})
+	if err != nil {
+		logger.Errf("sqlite auto migration error: %v", err)
+		return nil, err
+	}
+	return db, nil
+}
