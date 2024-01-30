6e95088b70167f94262b9c8de0bb39f1f11e2f01
Gian <50121981+zenndahl@users.noreply.github.com>
2024-01-30 08:03:33 -0300

    feat: handler

---
+++ handlers/handler.go
@@ -0,0 +1,17 @@
+package handlers
+
+import (
+	"gobooks/config"
+
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
