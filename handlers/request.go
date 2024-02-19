d692bd43025a2f7071b7884aa66f5e0e370d8f15
Gian <50121981+zenndahl@users.noreply.github.com>
2024-02-19 09:38:37 -0300

    fix: swagger is working now

--- handlers/request.go
+++ handlers/request.go
@@ -1,6 +1,9 @@
 package handlers
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func errParamRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
@@ -41,4 +44,37 @@ func (r *UpdateBookRequest) Validate() error {
 		return nil
 	}
 	return fmt.Errorf("at least one valid field must be provided")
+}
+
+type FinishBookRequest struct {
+	Name       string    `json:"name"`
+	Author     string    `json:"author"`
+	Genre      string    `json:"genre"`
+	Year       string    `json:"year"`
+	Finished   string    `json:"finished"`
+	FinishedAt time.Time `json:"finishedAt"`
+	Rating     int       `json:"rating"`
+}
+
+func (r *FinishBookRequest) Validate() error {
+	if r.Name == "" {
+		return errParamRequired("name", "string")
+	}
+	if r.Author == "" {
+		return errParamRequired("author", "string")
+	}
+	if r.Genre == "" {
+		return errParamRequired("genre", "string")
+	}
+	if r.Year == "" {
+		return errParamRequired("year", "bool")
+	}
+	if r.Finished == "" {
+		return errParamRequired("year", "bool")
+	}
+	if r.Rating < 0 || r.Rating > 10 {
+		return fmt.Errorf("param: rating (type: int) must be between 0 and 10")
+	}
+
+	return nil
 }
